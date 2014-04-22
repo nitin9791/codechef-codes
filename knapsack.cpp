486bebf9ed7c79fab529eaf5db5b1a23873237b5
Acer <acer@ubuntu.ubuntu-domain>
2014-04-23 03:16:18 +0530

    0/1 Knapshap problem

---
+++ knapsack.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+int max(int a, int b) { return (a > b)? a : b; }
+int knapSack(int W, int wt[], int val[], int n)
+{
+   int i, w;
+   int K[n+1][W+1];
+   for (i = 0; i <= n; i++)
+   {
+       for (w = 0; w <= W; w++)
+       {
+           if (i==0 || w==0)
+               K[i][w] = 0;
+           else if (wt[i-1] <= w)
+                 K[i][w] = max(val[i-1] + K[i-1][w-wt[i-1]],  K[i-1][w]);
+           else
+                 K[i][w] = K[i-1][w];
+       }
+   }
+
+   return K[n][W];
+}
+
+int main()
+{
+    int t,n,W,wt[1001],val[1001],ans,i;
+    cin>>t;
+    while(t--)
+    {
+        cin>>n;
+        cin>>W;
+        for(i=0;i<n;i++)
+            cin>>wt[i];
+        for(i=0;i<n;i++)
+            cin>>val[i];
+        ans=knapSack(W,wt,val,n);
+        cout<<ans<<"\n";
+    }
+    return 0;
+}
